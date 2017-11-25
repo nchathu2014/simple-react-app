9a9b2219556146071ba9e2a908fef1ae20b443ca
Nuwan Chathuranga <nuwan.chathuranga@pearson.com>
2017-11-25 11:44:41 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM,{render} from 'react-dom'
+import HelloWorld from './components/HelloWorld'
+
+render(
+    <HelloWorld />,
+    document.getElementById('app')
+);
