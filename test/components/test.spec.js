b2d7dbffe7c60185732ab8d98edba4eef757b53d
Nuwan Chathuranga <nuwan.chathuranga@pearson.com>
2017-12-12 17:38:12 +0530

    Test React15

---
+++ test/components/test.spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import expect from 'expect';
+
+//Testing basic component rendering
+test('render HelloWorld component', () => {
+    const wrapper = shallow(
+        <div className='hello-world'>
+            <h1>ReactJS Seed</h1>
+            <p>Welcome to ReactJS Seed</p>
+        </div>
+    );
+    expect(wrapper).toMatchSnapshot();
+});
